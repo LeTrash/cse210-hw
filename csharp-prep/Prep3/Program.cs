4937a8169142153bf070db6ba4ddbb7e2245c93f
J Clark <cla19072@byui.edu>
2023-01-10 21:20:19 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGen = new Random();
+        int mn = randomGen.Next(1,100);
+        int goose = 0;
+        int attempts = 0;
+
+        while (goose != mn){
+        Console.WriteLine("What is your guess?");
+        string guess = Console.ReadLine();
+        goose = int.Parse(guess);
+
+        if (goose < mn) {
+            Console.WriteLine("Guess higher");
+            attempts +=1;
+        }
+        else if (goose > mn){
+            Console.WriteLine("Guess lower");
+            attempts +=1;
+
+        }
+        else if (goose == mn){
+            attempts +=1;
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {attempts} guesses.");
+
+            break;
+        }
+ 
+        }
+
     }
 }
