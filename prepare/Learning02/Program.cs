fdad042945c137975d9283b7ec9dad53de10951c
J Clark <cla19072@byui.edu>
2023-01-18 11:01:33 -0700

    Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1999;
+        job1._endYear = 2020;
+        job1.DisplayJob();
+
+        Job job2 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "HR";
+        job1._startYear = 2002;
+        job1._endYear = 2023;
+        job2.DisplayJob();
+
+        Resume myResume = new Resume();
+        myResume._name = "Leon Smith";
+        myResume._jobs[0] = job1;
+        myResume._jobs[1] = job2;
+        myResume.DisplayResume();
+
     }
 }
